78198d425372b267d572e75bce17fad517ea4ca9
San Wan Aung <sanwanaung400042@gamil.com>
2023-02-14 12:10:42 +0630

    First commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const app = document.querySelector(".app");
+const userName = document.querySelector("#userName");
+const email = document.querySelector("#email");
+const age = document.querySelector("#age");
+
+const url = "http://localhost:3000/users";
+
+const registerUser = async () => {
+  app.innerHTML = "";
+  const user = userName.value;
+  const mail = email.value;
+  const ag = age.value;
+  const users = { name: user, email: mail, age: ag };
+  const resData = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(users),
+  });
+  userName.value = "";
+  email.value = "";
+  age.value = "";
+  const data = await resData.json();
+  updateUI(data);
+};
+
+const updateUser = (event) => {
+  console.log(event.target.id);
+};
+
+const deleteUser = async (event) => {
+  const deleteUserId = event.target.id;
+  console.log(deleteUserId);
+  const deleteToUser = { email: deleteUserId };
+  console.log(deleteToUser);
+  const response = await fetch(url, {
+    method: "DELETE",
+    body: JSON.stringify(deleteToUser),
+  });
+  const data = await response.json();
+  updateUI(data);
+};
+
+const updateUI = (userInfo) => {
+  const usersDiv = document.createElement("div");
+  for (let i = 0; i < userInfo.length; i++) {
+    const user = userInfo[i];
+    const userDiv = document.createElement("div");
+    userDiv.style.cssText = "margin-top: 8px";
+    userDiv.innerHTML = `
+        <div class="btn-container">
+            <div class="user-name">${user.name}</div>
+            <div class="btn-box">
+                <button type="button" class="btn btn-success" id="${user.email}" onclick="updateUser(event)">CreateUser</button>
+                <button type="button" class="btn btn-danger" id="${user.email}" onclick="deleteUser(event)">DeleteUser</button>
+            </div>
+        </div>
+    `;
+    usersDiv.append(userDiv);
+  }
+  app.append(usersDiv);
+};
+
+const fetchData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  updateUI(data);
+};
+
+fetchData();
---
+++ server.js
@@ -0,0 +1,78 @@
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+const users = [
+  { name: "san wan aung", email: "sanwanaung400042@gmail.com", age: 20 },
+  { name: "zaw mun aung", email: "zawmunaung089@gmail.com", age: 26 },
+  { name: "kyaw kyaw", email: "kyawkyaw@gmail.com", age: 30 },
+  { name: "aung aung", email: "aungaung@gmail.com", age: 20 },
+];
+
+const server = http.createServer((req, res) => {
+  const method = req.method;
+  const route = req.url;
+  const isRootUrl = route === "/";
+
+  if (isRootUrl) {
+    fs.readFile("index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (route === "/style.css") {
+    fs.readFile("style.css", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.write(data);
+      res.end();
+    });
+  } else if (route === "/script.js") {
+    fs.readFile("script.js", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.write(data);
+      res.end();
+    });
+  } else if (route === "/users") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(users));
+    } else if (method === "POST") {
+      let newData = "";
+      req.on("data", (chunk) => {
+        newData += chunk;
+      });
+      req.on("end", () => {
+        const changeObj = JSON.parse(newData);
+        console.log(changeObj);
+        users.push(changeObj);
+      });
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(users));
+    } else if (method === "DELETE") {
+      let dataFromScript = "";
+      let filterEmail;
+      req.on("data", (chunk) => {
+        dataFromScript += chunk;
+      });
+      req.on("end", () => {
+        console.log(dataFromScript);
+        const changeData = JSON.parse(dataFromScript);
+        console.log(changeData);
+        const deleteUserId = changeData.email;
+        filterEmail = users.filter((elem) => {
+          return elem.email !== deleteUserId;
+        });
+      });
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(filterEmail));
+    }
+    res.end();
+  } else {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Not Home Url");
+    res.end();
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server Started: Listenning on port ${port}`);
+});
